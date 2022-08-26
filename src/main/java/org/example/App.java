47bd14ad78e6c26525dc677ea98e298eab7cfc41
Обухов Алексей Сергеевич <Obuhov.Alexey@sogaz-med.ru>
2022-08-26 15:25:51 +0300

    hibernate lesson #9 (ManyToMany)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import org.example.model.Item;
-import org.example.model.Passport;
-import org.example.model.Person;
-import org.hibernate.PersistentObjectException;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,6 +9,7 @@ import org.hibernate.cfg.Configuration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 
 /**
  * Hello world!
@@ -20,35 +19,46 @@ public class App
 {
     public static void main( String[] args ) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Movie.class)
+                .addAnnotatedClass(Actor.class);
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();) {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-//            Person person = new Person("fff2");
+//            Movie movie = new Movie("Pulp fiction", 1994);
+//            Actor actor1 = new Actor("Harvey", 81);
+//            Actor actor2 = new Actor("Samuel", 81);
 //
-//            Passport passport = new Passport(78);
-//            person.setPassport(passport);//best practice
+//            movie.setActors(new ArrayList<>(List.of(actor1, actor1)));
+//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
 //
-//            session.save(person);
+//            session.save(movie);
+//            session.save(actor1);
+//            session.save(actor2);
 
 
-//            Person person = session.get(Person.class, 15);
-//            System.out.println(person.getPassport().getPassportNumber());
+
+//            Movie movie = session.get(Movie.class, 1);
+//            System.out.println(movie.getActors());
 
 
-//            Passport passport = session.get(Passport.class, 16);
-//            System.out.println(passport.getPerson().getName());
+
+//            Movie movie = new Movie("Reservoir Dogs", 1992);
+//            Actor actor = session.get(Actor.class, 1);
+//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
+//            actor.getMovies().add(movie);
+//            session.save(movie);
 
 
-//            Person person = session.get(Person.class, 15);
-//            person.getPassport().setPassportNumber(0);
 
-            Person person = session.get(Person.class, 15);
-            session.remove(person);
 
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+            Movie movieToRemove = actor.getMovies().get(0);
+            actor.getMovies().remove(movieToRemove);
+            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();    //в результате у person id = 1 устанавливается name = #2, что логично
 
